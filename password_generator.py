22d6406fca7163c487963266a2cc2a836f58b85d
vishnu  kumar singh <68421295+devil1329@users.noreply.github.com>
2020-12-03 22:21:37 +0530

    Create password_generator.py

---
+++ password_generator.py
@@ -0,0 +1,40 @@
+import random
+import tkinter as tk
+import string
+window=tk.Tk()
+window.geometry("600x200")
+window.title("**ViSpY**")
+
+def password_generator():
+    normal = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    symbols = """`~!@#$%^&*()_-+={}[]\|:;"'<>,.?/"""
+    chars= normal + symbols
+    password1 = ''
+    password1+=random.choice(string.ascii_lowercase)
+    name=random.randrange(12,32)
+    j=int(name/2)
+    for i in range(name-3):
+        if i==j:
+            password1+=random.choice(string.digits)
+        password1+=random.choice(chars)
+    password1+=random.choice(string.ascii_uppercase)
+    return password1
+
+def phrase_display():
+    greeting=password_generator()
+    greeting_display=tk.Text(master=window,height=6,width=25)
+    greeting_display.grid(column=0,row=4)
+    greeting_display.insert(tk.END,greeting)
+
+#for display    
+label1=tk.Label(text="Welcome to My App")
+label1.grid(column=1,row=0)
+label2=tk.Label(text="   ")
+label2.grid(column=0,row=2)
+label3=tk.Label(text="Click to Generate Random Passwords\t ")
+label3.grid()
+
+#for click button
+button1=tk.Button(text="         Click here         ",command=phrase_display)
+button1.grid(column=1,row=3)
+window.mainloop()
